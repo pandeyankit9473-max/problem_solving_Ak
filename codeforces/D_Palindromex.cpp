ccd354b82695b2b7940bf0e91dede5f971cfddf2
pandeyankit9473-max <pandeyankit9473@gmail.com>
2026-05-12 21:29:39 +0530

    Bss bhi ab ese aa gya hamesha yahi kru?

--- codeforces/D_Palindromex.cpp
+++ codeforces/D_Palindromex.cpp
@@ -1,43 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    int n; cin>>n;
+    n=2*n;
+    vector<int> arr(n), vis(n,0), index(n,0);
+    for(auto&i:arr) cin>>i;
+    // store index...
+    for(int i=0;i<n;i++) index[arr[i]]=i;
+     int ans =1;
+     for(int i=0;i<n;i++){
+        set<int>presentNum;
+        int flag=0;
+        int mex=0;
+        if(!vis[arr[i]]){
+            int left=i , right=index[arr[i]]; //its last posn.
+            while(left<=right){
+                if(arr[left]==arr[right]){
+                    presentNum.insert(arr[left]);
+                    vis[arr[left]]++;
+                    left++;
+                    right--;
+                }
+                // if pallindrome broked then...
+                else{
+                    flag=1;
+                    break;
+                }
+            }
+            // now we will cal mex by conting in set..
+            if(flag==0){
+            while(presentNum.count(mex)) mex++;
+            }
+
+        }
+        ans=max(ans,mex);
+     }
+     cout<<ans<<endl;
+}
+
 int main() {
+
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
     int t;
     cin >> t;
 
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<vector<int>> pos(n);
-
-        for (int i = 1; i <= 2 * n; i++) {
-            int x;
-            cin >> x;
-            pos[x].push_back(i);
-        }
-
-        int L = 0;
-        int R = 2 * n + 1;
-        int ans = 0;
-
-        for (int x = 0; x < n; x++) {
-            int p1 = pos[x][0];
-            int p2 = pos[x][1];
-
-            if (p1 > L && p2 < R) {
-                ans++;
-                L = p1;
-                R = p2;
-            } else {
-                break;
-            }
-        }
-
-        cout << ans << "\n";
+    while(t--) {
+        solve();
     }
 
     return 0;
